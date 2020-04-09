4e47510f5d172d8b9d0c245d34c161cfcf8a05bd
Carlos <traca@mail.com>
2020-04-09 14:06:48 -0300

    Adiçao de novo schemas

---
+++ backend/src/database/migrations/20200409123250_create_ingrediente.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('ingrediente', function (table) {
+        table.increments();
+        table.string('nome', 100).notNullable();
+        table.string('unidade', 10).notNullable();
+        table.boolean('derivado_leite').notNullable();
+        table.boolean('gluten').notNullable();
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('ingrediente');  
+};
