85fc743800194964902dd53547465729aeb7eecc
Carlos <traca@mail.com>
2020-04-22 18:58:22 -0300

    Adição de unidade

---
+++ backend/src/database/migrations/20200422170930_create_unidade.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('unidade', function (table) {
+        table.increments();
+        table.string('nome', 30).notNullable();
+        table.integer('id_tipo_unidade').notNullable();
+
+        table.foreign('id_tipo_unidade').references('id').inTable('tipo_unidade');
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('unidade');
+};
