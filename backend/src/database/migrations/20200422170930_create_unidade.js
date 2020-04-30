549cb0c10274427a106128de78d7b8d143c80cc9
andrevpk <andrevpk@gmail.com>
2020-04-30 19:23:31 -0300

    Inclusão de Unidades

--- backend/src/database/migrations/20200422170930_create_unidade.js
+++ backend/src/database/migrations/20200422170930_create_unidade.js
@@ -1,8 +1,8 @@
 exports.up = function(knex) {
     return knex.schema.createTable('unidade', function (table) {
         table.increments();
-        table.string('nome', 30).notNullable();
-        table.string('sigla', 30).notNullable();
+        table.string('nome', 100).notNullable();
+        table.string('sigla', 100).notNullable();
         table.integer('id_tipo_unidade').notNullable();
         table.integer('id_ingrediente');
         table.decimal('taxa_conversao', 8, 3).notNullable();
