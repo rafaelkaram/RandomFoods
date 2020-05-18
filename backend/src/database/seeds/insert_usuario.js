74c9dcf745068761abb7778dfa31c2655b2d2f5d
andrevpk <andrevpk@gmail.com>
2020-05-18 17:17:31 -0300

    Inserção de Ingrediente - Correção de Bugs

--- backend/src/database/seeds/insert_usuario.js
+++ backend/src/database/seeds/insert_usuario.js
@@ -5,27 +5,27 @@ exports.seed = function(knex) {
         {
             nome: 'André Vitor Kuduavski',
             email: 'andre.kuduavski@ufpr.br',
-            senha: '7d52f1dad957cc83a1b2e74977502087a51442de6c46acb9892b8b9542d5cc40'
+            senha: 'a650c7c95f7121a6610bc92654db19bd558d3f4e5e143232abef8be65bf6c846'
         },
         {
             nome: 'Carlos Felipe Godinho Silva',
             email: 'cfelipe@ufpr.br',
-            senha: '7d52f1dad957cc83a1b2e74977502087a51442de6c46acb9892b8b9542d5cc40'
+            senha: 'a650c7c95f7121a6610bc92654db19bd558d3f4e5e143232abef8be65bf6c846'
         },
         {
             nome: 'Gleidson dos Santos Novais',
             email: 'gleidison.santos@ufpr.br',
-            senha: '7d52f1dad957cc83a1b2e74977502087a51442de6c46acb9892b8b9542d5cc40'
+            senha: 'a650c7c95f7121a6610bc92654db19bd558d3f4e5e143232abef8be65bf6c846'
         },
         {
             nome: 'Guilherme Vinicius Valério',
             email: 'guilhermevalerio@ufpr.br',
-            senha: '7d52f1dad957cc83a1b2e74977502087a51442de6c46acb9892b8b9542d5cc40'
+            senha: 'a650c7c95f7121a6610bc92654db19bd558d3f4e5e143232abef8be65bf6c846'
         },
         {
             nome: 'Rafael Henrique Karam',
             email: 'rafaelkaram@ufpr.br',
-            senha: '7d52f1dad957cc83a1b2e74977502087a51442de6c46acb9892b8b9542d5cc40'
+            senha: 'a650c7c95f7121a6610bc92654db19bd558d3f4e5e143232abef8be65bf6c846'
         }
       ]);
     });
