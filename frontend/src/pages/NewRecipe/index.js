3001cfafd16ea6e2e1dc99db41118061ca10f335
andrevpk <andrevpk@gmail.com>
2020-08-27 23:34:04 -0300

    Cadastro de Receita

--- frontend/src/pages/NewRecipe/index.js
+++ frontend/src/pages/NewRecipe/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
-
+import {RadioButton,RadioGroup,ReversedRadioButton} from 'react-radio-buttons';
 import './styles.css';
 
 import logoImg from '../../assets/random_foods.png';
+import api from '../../services/api';
 
-export default function SignUp() {
+
+export default function NewRecipe() {
+    const [ ingredient, setIngredient ] = useState([]);
+    const [ ingredientTypes, setIngredientTypes] = useState([]);
+
+    useEffect(() => {
+        // api.get('ingrediente')
+        //     .then(response => {
+        //         setIngredient(response.data);
+        //     });
+        api.get('ingredientetype')
+            .then(response => {
+                setIngredientTypes(response.data);
+            })
+    }, []);
+
+
+
     return (
         <div className="new-recipe-container">
             <div className="content">
@@ -23,13 +41,38 @@ export default function SignUp() {
                 </section>
                 <form>
                     <input className="input-text" placeholder="Nome da Receita" />
+                    {/* <RadioGroup  horizontal>
+                    <RadioButton value="salgado">
+                        Salgado
+                    </RadioButton>
+                    <RadioButton value="doce">
+                        Doce
+                    </RadioButton>
+                    </RadioGroup>
                     <textarea placeholder="Descrição"/>
-                    <input className="input-text" placeholder="Tipo"/>
+                    <input className="input-text" placeholder="Tipo"/> */}
 
-                    <button className="button" type="submit">Cadastrar</button>
+                    {ingredientTypes.map(ingredientTypes =>{
+                        return(
+                            <div className="ingredients-grid">
+                                 <h5 key={ingredientTypes.tipo}>{ingredientTypes.tipo}</h5>
+                                 <img src={ingredientTypes.image_url + `massas-colored.svg`} alt={ingredientTypes.tipo}></img>
+                                 <div>
+                                    {ingredientTypes.ingredientes.map(ingredinete =>{
+                                        return(
+                                          <div
+                                             key={ingredinete.id}
+                                             value={ingredinete.id}>
+                                             {ingredinete.nome}
+                                          </div>  
+                                    )})}   
+                            </div>     
+                            </div>     
+                        )
+                    })}                  
                 </form>
+                <button className="button" type="submit">Cadastrar</button> 
             </div>
-    
         </div>
     );
 }
