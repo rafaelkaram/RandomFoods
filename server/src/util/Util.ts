c37a27914b0d6c1d16dbf17917cb2121a23c2834
Carlos F Godinho S <traca@mail.com>
2021-04-06 23:40:26 -0300

    Já nem sei mais

--- server/src/util/Util.ts
+++ server/src/util/Util.ts
@@ -6,11 +6,9 @@ export default {
     const networkInterfaces = os.networkInterfaces;
 
     const nets = networkInterfaces();
-    const results = Object.create(null);
 
     for (const name of Object.keys(nets)) {
         for (const net of nets[name]) {
-            // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
             if (net.family === 'IPv4' && !net.internal) {
                 return net.address;
             }
