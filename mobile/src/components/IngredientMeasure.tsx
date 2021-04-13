aeaafc1aeb66e4aa7a82a9fd26c97821ccf06fe1
Rafael Karam <rafael.karam10@gmail.com>
2021-04-12 21:33:45 -0300

    Measures - Combobox

--- mobile/src/components/IngredientMeasure.tsx
+++ mobile/src/components/IngredientMeasure.tsx
@@ -5,65 +5,24 @@ import { Input } from 'react-native-elements'
 import { IIngredient } from '../constants/interfaces';
 import DropDownPicker from 'react-native-dropdown-picker';
 import { ScrollView } from 'react-native-gesture-handler';
+import { Picker } from '@react-native-picker/picker';
 
 const IngredientMeasure = (props: { ingrediente: IIngredient, index: number }) => {
 
   const [semMedida, setSemMedida] = useState(true);
   const [quantidade, setQuantidade] = useState("");
+  const [medida, setMedida] = useState("");
+
 
   const ingrediente: IIngredient = props.ingrediente;
   const unidades = ingrediente.unidades;
 
-  if (ingrediente.tipoUnidade === 'UNIDADE') {
-    unidades.push({
-      id: 5,
-      nome: 'Unidade',
-      sigla: 'U',
-      taxaConversao: '1.000',
-      tipo: 'UNIDADE',
-    });
-  } else if (ingrediente.tipoUnidade === 'VOLUME') {
-    unidades.push({
-      id: 1,
-      nome: 'Litro',
-      sigla: 'L',
-      taxaConversao: '1.000',
-      tipo: 'VOLUME',
-    });
-    unidades.push({
-      id: 2,
-      nome: 'Mililitro',
-      sigla: 'Ml',
-      taxaConversao: '0.001',
-      tipo: 'VOLUME',
-    });
-  } else {
-    unidades.push({
-      id: 3,
-      nome: 'Miligrama',
-      sigla: 'Mg',
-      taxaConversao: '0.001',
-      tipo: 'PESO',
-    });
-    unidades.push({
-      id: 4,
-      nome: 'Grama',
-      sigla: 'g',
-      taxaConversao: '1.000',
-      tipo: 'PESO',
-    });
-    unidades.push({
-      id: 6,
-      nome: 'Quilograma',
-      sigla: 'Kg',
-      taxaConversao: '1000.000',
-      tipo: 'PESO',
-    });
-  }
+ 
 
   const comboBox = unidades.map(unidade => {
     return { label: unidade.sigla, value: unidade.id }
   });
+  console.log(comboBox);
 
   return (
 
@@ -71,27 +30,45 @@ const IngredientMeasure = (props: { ingrediente: IIngredient, index: number }) =
       key={ingrediente.id}
       style={styles.item}
     >
-      <Text style={{ lineHeight: 30 }}>{ingrediente.nome}</Text>
-      {/* <View style={styles.campos}> */}
-        <Input
-          placeholder="Qtd"
-          onChangeText={(value) => setQuantidade(value)}
-          value={quantidade}
-          containerStyle={styles.input}
-         inputContainerStyle={{ marginTop: 10, borderWidth: 1, width: 40, height: 30 }}
-        />
-        {!(ingrediente.tipoUnidade === 'UNIDADE' || ingrediente.semMedida) && (
-          <View style={{ zIndex: 999 - props.index }}>
-            <DropDownPicker
-              items={comboBox}
-              containerStyle={styles.comboBox}
-              dropDownStyle={{backgroundColor:"white",}}
-              onChangeItem={item => console.log(item.label, item.value)}
-            />
-          </View>
-        )}
+      <View style={styles.comp}>
+        <Text style={{ lineHeight: 30 }}>{ingrediente.nome}</Text>
+        <View style={styles.campos}>
+
+          {!(ingrediente.tipoUnidade === 'UNIDADE' || ingrediente.semMedida) && (
+            <View style={{ zIndex: 999 - props.index }}>
+              {/* <DropDownPicker
+                items={comboBox}
+                containerStyle={styles.comboBox}
+                dropDownStyle={{ backgroundColor: "white", }}
+                onChangeItem={item => console.log(item.label, item.value)}
+              /> */}
+              <Picker
+                selectedValue={medida}
+                style={styles.comboBox}
+                onValueChange={(itemValue) =>
+                  setMedida(itemValue)
+                }>
+                {unidades.map(item => {
+                  return (
+                    <Picker.Item key={item.id} label={item.sigla} value={item.id} />
+
+                  )
+                })}
+                {/* <Picker.Item label="Java" value="java" />
+                <Picker.Item label="JavaScript" value="js" /> */}
+              </Picker>
+            </View>
+          )}
+          <Input
+            placeholder="Qtd"
+            onChangeText={(value) => setQuantidade(value)}
+            value={quantidade}
+            containerStyle={styles.input}
+            inputContainerStyle={{ marginTop: 10, borderRadius: 3, borderWidth: 1, width: 40, height: 30, backgroundColor: 'white' }}
+          />
+        </View>
       </View>
-    // </View>
+    </View>
 
 
   );
@@ -105,25 +82,39 @@ const styles = StyleSheet.create({
   item: {
     flexDirection: "row",
     //  alignItems: "center",
-   // justifyContent: "flex-end",
-    margin: 10,
+    // justifyContent: "flex-end",
     padding: 15,
-    borderWidth: 1,
-    alignContent:"center",
-    alignItems:'center',  
+    backgroundColor: 'white',
+    margin: 5,
+    marginLeft: 10,
+    marginRight: 10,
+    borderRadius: 10,
+    width: 380,
+
+    //borderWidth: 1,
+    alignContent: "center",
+    alignItems: 'center',
 
   },
-  input:{
-    width:40,
-    marginTop:20,
+  input: {
+    width: 40,
+    marginTop: 20,
+    marginLeft: 10,
+    marginRight: 15,
+  },
+  comp: {
+    flexDirection: "column",
+    justifyContent: 'space-evenly',
+    alignContent: "center",
+    alignItems: 'center',
+    padding: 10,
+    //borderWidth:1,
+    width: 360,
+
+
   },
   campos: {
-  flexDirection:"row",
-  alignContent:"center",
-  alignItems:'center',
-  padding:10,
-  borderWidth:1,
-
+    flexDirection: "row",
   },
 
   comboBox: {
