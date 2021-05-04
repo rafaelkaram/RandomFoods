f82306b0591abfdfc2a29afcd0e35a281775e5c7
Carlos Godinho <cfelipe@ufpr.br>
2021-05-04 20:24:03 -0300

    Correção formato arquivo importado

--- server/src/config/uploadFiles.ts
+++ server/src/config/uploadFiles.ts
@@ -6,7 +6,9 @@ export default {
     destination: util.getPath('import'),
     filename: (request, file, cb) => {
       const { nome } = request.params;
-      const fileName = `${Date.now()}-${ nome ? nome.toUpperCase() : 'RECEITA' }`;
+      const originalName: string = file.originalname;
+      const extensao: string | undefined = originalName.split('.').pop();
+      const fileName = `${Date.now()}-${ nome ? nome.toUpperCase() : 'RECEITA' }.${ extensao }`;
 
       cb(null, fileName);
     }
