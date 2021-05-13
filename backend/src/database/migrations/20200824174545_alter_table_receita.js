2af175727efeab59226deb8f53a2dc592771696c
Carlos <traca.tm@gmail.com>
2021-05-12 22:24:24 -0300

    Remoção arquivos antigos

--- backend/src/database/migrations/20200824174545_alter_table_receita.js
+++
@@ -1,9 +0,0 @@
-exports.up = function(knex) {
-    return knex.schema.alterTable('receita', function (table) {
-        table.foreign('id_usuario').references('id').inTable('usuario');
-    });
-};
-
-exports.down = function(knex) {
-  
-};
