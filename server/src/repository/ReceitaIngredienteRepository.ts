93a24a668b1a8f264e8e72e443b59f3b9ca251dc
Carlos <traca.tm@gmail.com>
2021-05-25 23:31:18 -0300

    Indentação e ajuste de ordem

--- server/src/repository/ReceitaIngredienteRepository.ts
+++ server/src/repository/ReceitaIngredienteRepository.ts
@@ -10,36 +10,36 @@ export class ReceitaIngredienteRepository extends Repository<ReceitaIngrediente>
 	async findMatches(isPerfect: boolean, ids: number[], tempoPreparo: number, gluten: boolean, derivadoLeite: boolean, categorias: string[]): Promise<{ id: number }[]> {
 
 		const query = this.createQueryBuilder('ri')
-		.select('ri.receita.id', 'id')
-		.innerJoin('ri.receita', 'r')
-		.where('r.ativa = :ativa ', { ativa: true });
+			.select('ri.receita.id', 'id')
+			.innerJoin('ri.receita', 'r')
+			.where('r.ativa = :ativa ', { ativa: true });
 
 		if (ids && ids.length > 0)
-		query.andWhere('ri.ingrediente IN ( :...ids )', { ids });
+			query.andWhere('ri.ingrediente IN ( :...ids )', { ids });
 
 		if (gluten || derivadoLeite) {
-		query.andWhere(qb => {
-			const subQuery = qb.subQuery()
-			.select('ri2.receita.id')
-			.from(ReceitaIngrediente, 'ri2')
-			.from(Ingrediente, 'i')
-			.where(' ri2.ingrediente.id = i.id ');
-			if (gluten)
-			subQuery.andWhere('i.gluten = :gluten')
-			if (derivadoLeite)
-			subQuery.andWhere('i.derivadoLeite = :derivadoLeite');
+			query.andWhere(qb => {
+				const subQuery = qb.subQuery()
+				.select('ri2.receita.id')
+				.from(ReceitaIngrediente, 'ri2')
+				.from(Ingrediente, 'i')
+				.where(' ri2.ingrediente.id = i.id ');
+				if (gluten)
+				subQuery.andWhere('i.gluten = :gluten')
+				if (derivadoLeite)
+				subQuery.andWhere('i.derivadoLeite = :derivadoLeite');
 
-			subQuery.groupBy('ri2.receita.id')
-			.having('COUNT(ri2.*) = :countZero');
+				subQuery.groupBy('ri2.receita.id')
+				.having('COUNT(ri2.*) = :countZero');
 
-			const sub = subQuery.getQuery();
+				const sub = subQuery.getQuery();
 
-			return 'ri.receita.id IN (' + sub + ')';
-		});
+				return 'ri.receita.id IN (' + sub + ')';
+			});
 
-		if (gluten) query.setParameter('gluten', true);
-		if (derivadoLeite) query.setParameter('derivadoLeite', true);
-		query.setParameter('countZero', 0);
+			if (gluten) query.setParameter('gluten', true);
+			if (derivadoLeite) query.setParameter('derivadoLeite', true);
+			query.setParameter('countZero', 0);
 
 		}
 
