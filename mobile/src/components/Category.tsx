a3088843b57e2292e7feee99b15b826ea79c3c16
andrevpk <andrevpk@gmail.com>
2021-08-04 00:06:19 -0300

    Ajuste Backend e Frontend - Imagens de Categoria

--- mobile/src/components/Category.tsx
+++ mobile/src/components/Category.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, View } from 'react-native';
 
 import styles from '../styles/components/Category';
-import globalStyles from '../styles/Global';
 
 const Category = (props: { nome: string }) => {
     return (
-        <View style={ styles.categoryContainer }>
-            <Text style={[ globalStyles.regularText, styles.categoryText ]}>{ props.nome }</Text>
+        <View style={[styles.categoryContainer]}>
+
+            <Image
+                source={{ uri: props.nome }}
+                style={{ width: 40, height: 40 }}
+            />
+
         </View>
+
     )
 }
 
